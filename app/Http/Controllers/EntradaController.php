c925fcc124e324fc8e8bd543af3f58dd319fde01
euleoh <shaolinmatadordeporcotyui@gmail.com>
2026-01-27 15:47:05 -0300

    Atualizado o CRUD de clientes e produtos

--- app/Http/Controllers/EntradaController.php
+++ app/Http/Controllers/EntradaController.php
@@ -3,18 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\Entrada;
+use App\Models\Produto;
 use Illuminate\Http\Request;
 
 class EntradaController extends Controller
 {
     public function store(Request $request){
-        
-    $entrada = Entrada::create([
+    $produto = Produto::find($request->id_produto);  
+    
+    if(!$produto){
+        return response()-> json(['message'=> 'Produto não encontrado']);
+    } else {
+        $entrada = Entrada::create([
         'id_produto'=> $request-> id_produto,
         'quantidade'=> $request-> quantidade
         ]);
 
+    }
+
+    if(isset($entrada)){
+        $produto-> estoque += $entrada->quantidade;
+    }
+    
+    $produto-> update();
         return response()-> json($entrada);
+        
     }
 
     public function index(){
@@ -26,11 +39,23 @@ class EntradaController extends Controller
     public function delete($id){
         $entrada = Entrada::find($id);
 
-        if(!$entrada){
+        if($entrada == null){
             return response()-> json(['message'=> 'Produto não encontrado']);
         }
 
-        $entrada-> delete();
+        $produto = $entrada-> id_produto;
+
+        $quantidade= $entrada-> quantidade;
+
+        $produto = Produto::find($produto);
+
+        if(isset($produto)){
+            $produto-> quantidade_estoque -= $quantidade;
+        }
+
+        $produto-> update();
+
+        $entrada-> delete($id);
 
         return response()-> json(['Produto deletado']);
     }
